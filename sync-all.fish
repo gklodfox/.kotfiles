d6d5eca596fa5807daeba77712f49da02f86e04e
gklodfox <gklodkowski@gmail.com>
2025-10-13 12:42:36 +0200

    sync

--- sync-all.fish
+++ sync-all.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 
 cd ~/.kotfiles
-for dir in fox-*
+for dir in fox-*-*
         cd ~/.kotfiles/$dir
         echo "UPDATE: $PWD"
+	git pull --rebase
         git add .
         git commit -m "sync" --allow-empty
         git push

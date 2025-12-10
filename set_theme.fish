8de8e7b31056993aa3ff56fee282b477301502a4
gklodfox <gklodkowski@gmail.com>
2025-12-10 04:22:25 +0100

    upd

--- set_theme.fish
+++ set_theme.fish
@@ -43,3 +43,43 @@ if test -f $FOX_TMUX_CONFIG/tmux.conf
     tmux source-file $TMUX_FLOW_TARGET_FILE
     tmux source-file $FOX_TMUX_CONFIG/tmux.conf
 end
+
+# $ set -U fish_color_normal normal
+# $ set -U fish_color_command ff0087
+# $ set -U fish_color_quote 005fd7
+# $ set -U fish_color_redirection 8787ff --bold
+# $ set -U fish_color_end ff0000
+# $ set -U fish_color_error brred
+# $ set -U fish_color_param ff87d7
+# $ set -U fish_color_comment 870087
+# $ set -U fish_color_selection white --bold --background=brblack
+# $ set -U fish_color_search_match white --bold --background=brblack
+# $ set -U fish_color_history_current --bold
+# $ set -U fish_color_operator brcyan
+# $ set -U fish_color_escape brcyan
+# $ set -U fish_color_cwd green
+# $ set -U fish_color_cwd_root red
+# $ set -U fish_color_valid_path --underline=single
+# $ set -U fish_color_autosuggestion 8700af
+# $ set -U fish_color_user brgreen
+# $ set -U fish_color_host normal
+# $ set -U fish_color_host_remote yellow
+# $ set -U fish_color_history_current --bold
+# $ set -U fish_color_status red
+# $ set -U fish_color_cancel --reverse
+# $ set -U fish_pager_color_background
+# $ set -U fish_pager_color_prefix normal --bold --underline=single
+# $ set -U fish_pager_color_progress brwhite --bold --background=cyan
+# $ set -U fish_pager_color_completion normal
+# $ set -U fish_pager_color_description yellow --italics
+# $ set -U fish_pager_color_selected_background --reverse
+# $ set -U fish_pager_color_selected_prefix
+# $ set -U fish_pager_color_selected_completion
+# $ set -U fish_pager_color_selected_description
+# $ set -U fish_pager_color_secondary_description
+# $ set -U fish_color_keyword
+# $ set -U fish_pager_color_secondary_prefix
+# $ set -U fish_color_option
+# $ set -U fish_pager_color_secondary_background
+# $ set -U fish_pager_color_secondary_completion
+
